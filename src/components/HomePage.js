3abd9050b7a98f6f8ced341641df74784c49a472
Dan Lorch <dan.lorch@gmail.com>
2018-03-19 20:13:24 -0600

    html files

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,27 +3,32 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Jenny M&#39;s Longarm Quilting</h1>
-
-      <h2>Start Quilting :)</h2>
-
-      <div className="row">
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-          Testing Bootstrap
-        </div>
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-          Testing Bootstrap
-        </div>
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-          Testing Bootstrap
-        </div>
-      </div>
-
-      <div>
-        <h2>Longarm Machine Quilting</h2>
-      </div>
-    </div>
+    <div className="container-fluid">
+    	<div class="row containerSpace">
+			<section className="col text-center">
+				<h1>LONGARM QUILTING SERVICES</h1>
+				<p>Let us finish your quilt top with a delicious texture!  Our machine quilting services are available by mail.  Send us your quilt top and backing.  We&rsquo;ll quilt your special project in our home studio, giving it all the love and attention to detail that is my standard.  You&rsquo;ll receive it back soon, beautifully presented and ready to be bound. </p>
+				<h5>THANK YOU FOR USING JENNY M&rsquo;S QUILTING CO.!</h5>
+			</section> {/* end section*/} 
+		</div> {/* end row*/}
+		<div class="row containerSpace">
+			<section className="col">
+				<img src="../images/thewarmcompanywarmnaturalcottonblendbatting_aiid2055405.jpg" alt="test image" height="100" width="100"/>
+				<h2>CLEAR TURN AROUND</h2>
+				<p>You are trusting us with a beloved project!  We commit to clear and transparent turn around with updates along the way.  Know when your quilt will be completed upfront!</p>
+			</section> {/* end section*/} 
+			<section className="col">
+				<img src="../images/thewarmcompanywarmnaturalcottonblendbatting_aiid2055405.jpg" alt="test image" height="100" width="100"/>
+				<h2>BATTING INCLUDED</h2>
+				<p>No need to ship batting.  We provide Quilter&rsquo;s Dream Select batting.  You may also choose to send your preferred batting.  Thread is included in our services.</p>
+			</section> {/* end section*/} 
+			<section className="col">
+				<img src="../images/thewarmcompanywarmnaturalcottonblendbatting_aiid2055405.jpg" alt="test image" height="100" width="100"/>
+				<h2>BATTING INCLUDED</h2>
+				<p>No need to ship batting.  We provide Quilter&rsquo;s Dream Select batting.  You may also choose to send your preferred batting.  Thread is included in our services.</p>
+			</section> {/* end section*/}   
+		</div> {/* end row*/}  
+    </div> 
   );
 };
 
